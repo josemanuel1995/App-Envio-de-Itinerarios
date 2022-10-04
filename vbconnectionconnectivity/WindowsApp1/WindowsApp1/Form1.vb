b41343676ab224b57e7d3ae1a17342f19434bd95
José Manuel <51034322+josemanuel1995@users.noreply.github.com>
2022-10-03 23:28:31 -0500

    Add files via upload

---
+++ vbconnectionconnectivity/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim i As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Documentos de Trabajos\Prueba admision -newshore\Talleres de newshore\Problema 5\vbconnectionconnectivity\WindowsApp1\WindowsApp1\Database1.mdf;Integrated Security=True"
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        disp_data()
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into table1 values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "')"
+        cmd.ExecuteNonQuery()
+
+        disp_data()
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+
+        MessageBox.Show("Record Inserted Successfully")
+
+    End Sub
+
+
+    Public Sub disp_data()
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from table1"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "update table1 set Fecha_de_ida='" + TextBox2.Text + "',Fecha_de_vuelta='" + TextBox3.Text + "',Pasajero_1='" + TextBox4.Text + "',Pasajero_2='" + TextBox5.Text + "',Numero_Contacto='" + TextBox6.Text + "',Email_Contacto='" + TextBox7.Text + "' where PNR=" & i & ""
+        cmd.ExecuteNonQuery()
+
+        disp_data()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from table1 where PNR='" + TextBox1.Text + "'"
+        cmd.ExecuteNonQuery()
+
+        disp_data()
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        disp_data()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from table1 where PNR='" + TextBox1.Text + "'"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+        Try
+            If con.State = ConnectionState.Open Then
+                con.Close()
+
+            End If
+            con.Open()
+
+            i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+
+            cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from table1 where PNR=" & i & ""
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            Dim dr As SqlClient.SqlDataReader
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While dr.Read
+
+                TextBox1.Text = dr.GetString(1).ToString()
+                TextBox2.Text = dr.GetString(2).ToString()
+                TextBox3.Text = dr.GetString(3).ToString()
+                TextBox4.Text = dr.GetString(4).ToString()
+                TextBox5.Text = dr.GetString(5).ToString()
+                TextBox6.Text = dr.GetString(6).ToString()
+                TextBox7.Text = dr.GetString(7).ToString()
+
+            End While
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
